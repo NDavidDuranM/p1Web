fd3e886db584d7c8d3da1fcf2c099fdd26ab5d5a
NDavidDuranM <ndaviddm@gmail.com>
2021-09-27 23:22:29 -0400

    first commit

---
+++ src/main/java/com/pucmm/isc517/Isc517Application.java
@@ -0,0 +1,13 @@
+package com.pucmm.isc517;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isc517Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isc517Application.class, args);
+	}
+
+}
